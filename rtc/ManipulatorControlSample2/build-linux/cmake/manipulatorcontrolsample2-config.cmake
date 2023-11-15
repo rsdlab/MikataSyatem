24320e46224e46d57a83aa53381cfbd7740974c2
FukutaManato <142777914+FukutaManato@users.noreply.github.com>
2023-11-15 16:55:45 +0900

    Add files via upload

---
+++ rtc/ManipulatorControlSample2/build-linux/cmake/manipulatorcontrolsample2-config.cmake
@@ -0,0 +1,54 @@
+# ManipulatorControlSample2 CMake config file
+#
+# This file sets the following variables:
+# ManipulatorControlSample2_FOUND - Always TRUE.
+# ManipulatorControlSample2_INCLUDE_DIRS - Directories containing the ManipulatorControlSample2 include files.
+# ManipulatorControlSample2_IDL_DIRS - Directories containing the ManipulatorControlSample2 IDL files.
+# ManipulatorControlSample2_LIBRARIES - Libraries needed to use ManipulatorControlSample2.
+# ManipulatorControlSample2_DEFINITIONS - Compiler flags for ManipulatorControlSample2.
+# ManipulatorControlSample2_VERSION - The version of ManipulatorControlSample2 found.
+# ManipulatorControlSample2_VERSION_MAJOR - The major version of ManipulatorControlSample2 found.
+# ManipulatorControlSample2_VERSION_MINOR - The minor version of ManipulatorControlSample2 found.
+# ManipulatorControlSample2_VERSION_REVISION - The revision version of ManipulatorControlSample2 found.
+# ManipulatorControlSample2_VERSION_CANDIDATE - The candidate version of ManipulatorControlSample2 found.
+
+message(STATUS "Found ManipulatorControlSample2-1.0.0")
+set(ManipulatorControlSample2_FOUND TRUE)
+
+find_package(<dependency> REQUIRED)
+
+#set(ManipulatorControlSample2_INCLUDE_DIRS
+#    "/usr/local/include/manipulatorcontrolsample2-1"
+#    ${<dependency>_INCLUDE_DIRS}
+#    )
+#
+#set(ManipulatorControlSample2_IDL_DIRS
+#    "/usr/local/include/manipulatorcontrolsample2-1/idl")
+set(ManipulatorControlSample2_INCLUDE_DIRS
+    "/usr/local/include/"
+    ${<dependency>_INCLUDE_DIRS}
+    )
+set(ManipulatorControlSample2_IDL_DIRS
+    "/usr/local/include//idl")
+
+
+if(WIN32)
+    set(ManipulatorControlSample2_LIBRARIES
+        "/usr/local//libmanipulatorcontrolsample2.a"
+        ${<dependency>_LIBRARIES}
+        )
+else(WIN32)
+    set(ManipulatorControlSample2_LIBRARIES
+        "/usr/local//libmanipulatorcontrolsample2.so"
+        ${<dependency>_LIBRARIES}
+        )
+endif(WIN32)
+
+set(ManipulatorControlSample2_DEFINITIONS ${<dependency>_DEFINITIONS})
+
+set(ManipulatorControlSample2_VERSION 1.0.0)
+set(ManipulatorControlSample2_VERSION_MAJOR 1)
+set(ManipulatorControlSample2_VERSION_MINOR 0)
+set(ManipulatorControlSample2_VERSION_REVISION 0)
+set(ManipulatorControlSample2_VERSION_CANDIDATE )
+
---
+++ rtc/ManipulatorControlSample2/build-linux/src/CMakeFiles/ManipulatorControlSample2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ManipulatorControlSample2.dir/ManipulatorControlSample2.cpp.o"
+  "ManipulatorControlSample2.pdb"
+  "ManipulatorControlSample2.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ManipulatorControlSample2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
