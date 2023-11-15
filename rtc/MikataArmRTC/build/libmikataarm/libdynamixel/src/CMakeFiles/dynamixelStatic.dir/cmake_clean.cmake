c8e7d059cc339a272b1ef1da7abdac16d4e1ef12
FukutaManato <142777914+FukutaManato@users.noreply.github.com>
2023-11-15 17:13:54 +0900

    Add files via upload

---
+++ rtc/MikataArmRTC/build/libmikataarm/libdynamixel/src/CMakeFiles/dynamixelStatic.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dynamixelStatic.dir/DynamixelV1.cpp.o"
+  "CMakeFiles/dynamixelStatic.dir/DynamixelV2.cpp.o"
+  "libdynamixelStatic.a"
+  "libdynamixelStatic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dynamixelStatic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ rtc/MikataArmRTC/build/libmikataarm/libdynamixel/src/CMakeFiles/dynamixel_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dynamixel_test.dir/test.cpp.o"
+  "dynamixel_test"
+  "dynamixel_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dynamixel_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
